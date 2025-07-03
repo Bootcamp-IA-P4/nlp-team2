cafae66eca542553f150925189c7597c1febac68
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-07-03 15:09:21 +0200

    feat(config): add description

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -3,5 +3,6 @@ class Setting:
     def __init__(self):
         self.title = "Modzilla"
         self.version = "v1"
+        self.description = "API para la moderación de comentarios de YouTube"
 
 setting = Setting()
