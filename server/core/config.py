f56a5fec33689a44d9f6e8db8dc3d69ebcbf9750
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-07-04 11:03:13 +0200

    feat(config): add authors

--- server/core/config.py
+++ server/core/config.py
@@ -4,5 +4,6 @@ class Setting:
         self.title = "Modzilla"
         self.version = "v1"
         self.description = "API para la moderación de comentarios de YouTube"
+        self.authors = ["Fernando Garcia Catalan", "Juan Carlos Macias", "Alejandro Rajado Martin", "Vada Velazquez"]
 
 setting = Setting()
