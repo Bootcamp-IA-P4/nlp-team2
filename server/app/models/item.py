1ac8752cec8a0da7c8b46745600543860f0941c3
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-07-04 09:59:34 +0200

    feat(main): ADD api title, version and description

--- server/app/models/item.py
+++ server/app/models/item.py
@@ -1,7 +1,3 @@
 from pydantic import BaseModel
 
-class Item(BaseModel):
-    id: int
-    nombre: str
-    descripcion: str = None
-    precio: float
+
--- server/main.py
+++ server/main.py
@@ -1,5 +1,9 @@
-import database.db_manager as database
-import scraper.scrp as scrp
+from fastapi import FastAPI
+import server.database.db_manager as database
+import server.scraper.scrp as scrp
+from app.core.config import setting
+
+
 
 print   ("🟢 Iniciando la extracción de comentarios de YouTube..."  )
 retorno = scrp.scrape_youtube_comments("https://www.youtube.com/watch?v=8kZMBVvK-gg")
@@ -7,3 +11,15 @@ retorno = scrp.scrape_youtube_comments("https://www.youtube.com/watch?v=8kZMBVvK
 database.insert_video_from_scrapper(retorno)
 
 print("🟢 Extracción completada."  )
+
+
+app = FastAPI(
+    title=setting.title,
+    version=setting.version,
+    description=setting.description
+    )
+
+
+@app.get(f"/{setting.version}/prediction_request")
+def read_root(data: dict):
+    return {"mensaje": data["url"]}
