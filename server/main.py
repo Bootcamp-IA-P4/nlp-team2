f000282c7d98e0cf1255f2c9de1b99e74dae0cdc
unknown <alexrajado@gmail.com>
2025-07-08 11:42:20 +0200

    fix: add frontend Render URL to CORS config

--- server/main.py
+++ server/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173","https://nlp-team2-front.onrender.com"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
